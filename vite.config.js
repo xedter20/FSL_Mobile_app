38f34861e87e308de54a9744352f7269901b7848
xedter20 <dextermiranda441@gmail.com>
2024-11-15 14:14:57 +0800

    updates

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  server: {
+    host: '0.0.0.0', // Listen on all interfaces
+    port: 3000, // Optional: Set a specific port if you want
+    hmr: {
+      overlay: false // Disables the error overlay
+    }
+  },
+  plugins: [react()]
+  // Check if you have a `base` property or other URL-related config
+});
